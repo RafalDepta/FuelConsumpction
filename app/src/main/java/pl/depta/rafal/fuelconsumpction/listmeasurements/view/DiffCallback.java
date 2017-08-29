7e7c08afb310d26f51dd13e418f5ff200f732077
Rafal Depta <rafal.depta@sim.com.pl>
2017-08-29 15:38:02 +0200

    - clean DiffUtils

---
+++ app/src/main/java/pl/depta/rafal/fuelconsumpction/listmeasurements/view/DiffCallback.java
@@ -0,0 +1,51 @@
+package pl.depta.rafal.fuelconsumpction.listmeasurements.view;
+
+import android.support.v7.util.DiffUtil;
+
+import java.util.List;
+import java.util.Objects;
+
+import pl.depta.rafal.fuelconsumpction.model.Measurement;
+
+
+public class DiffCallback extends DiffUtil.Callback {
+    private List<? extends Measurement> oldList;
+    private List<? extends Measurement> newList;
+
+    DiffCallback(List<? extends Measurement> oldList, List<? extends Measurement> newList) {
+        this.oldList = oldList;
+        this.newList = newList;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldList.get(oldItemPosition).getId() ==
+                newList.get(newItemPosition).getId();
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        Measurement measurement = newList.get(newItemPosition);
+        Measurement old;
+        if (oldItemPosition >= newList.size()) {
+            return false;
+        } else {
+            old = newList.get(oldItemPosition);
+            return measurement.getId() == old.getId()
+                    && Objects.equals(measurement.getDate(), old.getDate())
+                    && Objects.equals(measurement.getDistance(), old.getDistance())
+                    && Objects.equals(measurement.getFuelPrice(), old.getFuelPrice())
+                    && Objects.equals(measurement.getFuelConsumption(), old.getFuelConsumption());
+        }
+    }
+}
--- app/src/main/java/pl/depta/rafal/fuelconsumpction/listmeasurements/view/MeasurementAdapter.java
+++ app/src/main/java/pl/depta/rafal/fuelconsumpction/listmeasurements/view/MeasurementAdapter.java
@@ -6,8 +6,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
-import com.google.common.collect.Lists;
-
 import java.util.List;
 import java.util.Objects;
 
@@ -20,55 +18,18 @@ class MeasurementAdapter extends RecyclerView.Adapter<MeasurementAdapter.Measure
 
     private List<? extends Measurement> mMeasurementList;
 
-    MeasurementAdapter() {
-
+    Measurement getMeasurementAt(int position) {
+        return mMeasurementList.get(position);
     }
 
     void setMeasurementList(final List<? extends Measurement> measurementList) {
         if (mMeasurementList == null) {
-            mMeasurementList = measurementList;
             notifyItemRangeInserted(0, measurementList.size());
         } else {
-            DiffUtil.DiffResult result = DiffUtil.calculateDiff(new DiffUtil.Callback() {
-                @Override
-                public int getOldListSize() {
-                    return mMeasurementList.size();
-                }
-
-                @Override
-                public int getNewListSize() {
-                    return measurementList.size();
-                }
-
-                @Override
-                public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
-                    return mMeasurementList.get(oldItemPosition).getId() ==
-                            measurementList.get(newItemPosition).getId();
-                }
-
-                @Override
-                public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
-                    Measurement measurement = measurementList.get(newItemPosition);
-                    Measurement old;
-                    if (oldItemPosition >= measurementList.size()) {
-                        return false;
-                    } else {
-                        old = measurementList.get(oldItemPosition);
-                        return measurement.getId() == old.getId()
-                                && Objects.equals(measurement.getDate(), old.getDate())
-                                && Objects.equals(measurement.getDistance(), old.getDistance())
-                                && Objects.equals(measurement.getFuelPrice(), old.getFuelPrice())
-                                && Objects.equals(measurement.getFuelConsumption(), old.getFuelConsumption());
-                    }
-                }
-            });
-            mMeasurementList = measurementList;
+            DiffUtil.DiffResult result = DiffUtil.calculateDiff(new DiffCallback(mMeasurementList, measurementList));
             result.dispatchUpdatesTo(this);
         }
-    }
-
-    Measurement getMeasurementAt(int position) {
-        return mMeasurementList.get(position);
+        mMeasurementList = measurementList;
     }
 
     @Override
